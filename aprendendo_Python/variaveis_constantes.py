8ad3aedd754e067942ac260ff909b82534d0c6a0
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-05-24 17:01:46 -0300

    variaveis e constantes

---
+++ aprendendo_Python/variaveis_constantes.py
@@ -0,0 +1,14 @@
+name = "Lucas"
+age = 10
+
+name , age = "Renan" , 27
+
+print(name, age)
+
+limite_saque_diario = 1000
+
+print (limite_saque_diario)
+
+STATES = ["SP", "RJ", "SC", "RS"]
+
+print(STATES)
