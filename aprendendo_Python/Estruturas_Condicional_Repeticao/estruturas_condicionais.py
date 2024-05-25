80c938942a86b3d10e1cda47f8e600c2229bf89d
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-05-25 13:40:03 -0300

    Estruturas condicionais

---
+++ aprendendo_Python/Estruturas_Condicional_Repeticao/estruturas_condicionais.py
@@ -0,0 +1,16 @@
+valor1 = 200
+valor2 = 300
+
+if valor1 >= valor2:
+    print("valor 1 é maior que valor 2")
+else:
+    print("valor 2 é maior que valor 1")
+
+
+print("Insira sua idade: ")
+idade = int(input())
+
+if idade >= 18:
+    print("Você é de maior")
+else:
+    print("Você é de menor")
