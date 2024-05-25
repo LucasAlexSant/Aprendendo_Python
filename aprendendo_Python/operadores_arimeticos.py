e6182c0b541bd4e80eed1768b6a71aaa6097c744
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-05-25 12:42:46 -0300

    Tipos de Operadores com Python

---
+++ aprendendo_Python/operadores_arimeticos.py
@@ -0,0 +1,15 @@
+produto_1 = 20
+produto_2 = 10
+
+print (produto_1 + produto_2) #adição
+print (produto_1 - produto_2) #subtração
+print (produto_1 / produto_2) #divisão
+print (produto_1 * produto_2) #multiplicação
+print (produto_1 % produto_2) #resto de divisão
+print (produto_1 ** produto_2)#exponencialmente 
+
+x= (10 + 5) * 4
+y= 10 / 2 + 25 * 2 - 2 ** 2
+
+print(x)
+print(y)
