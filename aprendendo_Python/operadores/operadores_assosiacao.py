a4903d34ec764f47ae139fa17127079cb2fdbf13
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-05-25 13:10:53 -0300

    Operadores de associação

---
+++ aprendendo_Python/operadores/operadores_assosiacao.py
@@ -0,0 +1,9 @@
+frutas = ["laranja", "melancia" ]
+curso = "curso de python"
+
+print("laranja" in frutas)
+print("laranja" not in frutas)
+print("maça" in frutas)
+print("maça" not in frutas)
+
+print("python" in curso)
