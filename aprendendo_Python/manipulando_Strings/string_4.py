36eec76565ca71fa617954716d5b307497d64371
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-05-28 21:44:13 -0300

    Fatiamento de string

---
+++ aprendendo_Python/manipulando_Strings/string_4.py
@@ -0,0 +1,20 @@
+nome = "Lucas"
+
+mensagem = f""" 
+    Olá meu nome é {nome}, 
+Eu estou aprendendo Python. 
+     Essa mensagem tem diferentes recuos;
+"""
+
+
+print(mensagem)
+
+print("""
+    ======== MENU =========
+      
+      1 - DEPOSITAR
+      2 - SACAR 
+      0 - SAIR
+    =======================
+            OBRIGADO POR USAR NOSSO SISTEMAS!!!
+""")
